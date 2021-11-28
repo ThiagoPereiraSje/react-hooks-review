4995f594d8e1c558fb4b8d89cd6f0e5bfb459891
Thiago Pereira <thiagopereiramail@gmail.com>
2021-11-28 21:36:45 +0000

    Run effects only once

--- use-effect/src/App.js
+++ use-effect/src/App.js
@@ -1,13 +1,15 @@
 // import ClassCounterOne from "./components/ClassCounterOne";
 // import HookCounterOne from "./components/HookCounterOne";
-import HookCounterOne02 from "./components/HookCounterOne02";
+// import HookCounterOne02 from "./components/HookCounterOne02";
+import HookMouse from "./components/HookMouse";
 
 function App() {
   return (
     <div>
       {/* <ClassCounterOne /> */}
       {/* <HookCounterOne /> */}
-      <HookCounterOne02 />
+      {/* <HookCounterOne02 /> */}
+      <HookMouse />
     </div>
   );
 }
